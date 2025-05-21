bdad7bc61f50e2d75f4eabd5c9810570030dbdd9
fahimjh001 <fahimjh001@gmail.com>
2025-05-22 00:01:48 +0600

    basic functionality for array box added

---
+++ logic.js
@@ -0,0 +1,8 @@
+let arrayBox = document.querySelector(".array");
+let searchBox = document.querySelector(".search");
+let sortBox = document.querySelector(".sort");
+let listBox = document.querySelector(".list");
+
+arrayBox.addEventListener("click",()=>{
+    window.location.href="array.html";
+});
