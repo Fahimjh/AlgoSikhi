b60fcade4ea9fc64b80049583aa9783b04233df0
fahimjh001 <fahimjh001@gmail.com>
2025-05-22 18:33:55 +0600

    array introduction basic part1 added

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
-let arrayBox = document.querySelector(".array");
-let searchBox = document.querySelector(".search");
+let arrayBox = document.querySelector(".arrayIntro");
+let searchBox = document.querySelector(".Updation");
 let sortBox = document.querySelector(".sort");
-let listBox = document.querySelector(".list");
+// let listBox = document.querySelector(".list");
 
 arrayBox.addEventListener("click",()=>{
     window.location.href="array.html";
